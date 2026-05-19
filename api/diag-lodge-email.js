86f95cf53699e2770679f647f1ad1aa12360cd52
Andrew Vaughan <andrew@ridedownsouth.com>
2026-05-19 12:18:22 +0000

    Remove lodge email diagnostic endpoint

--- api/diag-lodge-email.js
+++
@@ -1,6 +0,0 @@
-import { zohoApi } from './_zoho.js'
-export default async function(req, res) {
-  const name = req.query.name || 'Bahnhof'
-  const r = await zohoApi('GET', 'CustomModule5/search?criteria=(Name:contains:' + name + ')&fields=id,Name,Email,Preferred_Email,Email_Reservations_2&per_page=5')
-  res.status(200).json(r?.data || [])
-}
